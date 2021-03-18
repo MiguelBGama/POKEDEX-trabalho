b89683b9602bae9561c775acc7d75132819a4a63
Miguel <57196019+MiguelBGama@users.noreply.github.com>
2021-03-18 10:19:11 -0300

    Initial commit

---
+++ Pokedex/src/pokedex/Pokedex.java
@@ -0,0 +1,16 @@
+package pokedex;
+
+/**
+ *
+ * @author Grupo 1 da Materia de Programação Orientada a Objeto
+ */
+public class Pokedex {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
