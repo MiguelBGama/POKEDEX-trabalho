8d593057f0fdfc77399f91d8311953b3f6b5350e
Miguel <57196019+MiguelBGama@users.noreply.github.com>
2021-03-24 14:00:19 -0300

    Adicionando getters e setters

--- Pokedex/src/pokedex/Pokedex.java
+++ Pokedex/src/pokedex/Pokedex.java
@@ -1,14 +1,7 @@
 package pokedex;
 
-/**
- *
- * @author Grupo 1 da Materia de Programação Orientada a Objeto
- */
-public class Pokedex {
 
-    /**
-     * @param args the command line arguments
-     */
+public class Pokedex {
     public static void main(String[] args) {
         // TODO code application logic here
     }
--- Pokedex/src/pokedex/pokemon.java
+++ Pokedex/src/pokedex/pokemon.java
@@ -8,5 +8,32 @@ public class pokemon {
     String nome;
     String descricao;
     String tipo;
+
+    public pokemon() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
     
 }
