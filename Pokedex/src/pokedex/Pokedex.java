1e901e32463625b073af15ad7387f3622b096f75
Miguel <57196019+MiguelBGama@users.noreply.github.com>
2021-04-07 17:26:17 -0300

    ideias para a organização e estruturação da classe Pokedex

--- Pokedex/src/pokedex/Pokedex.java
+++ Pokedex/src/pokedex/Pokedex.java
@@ -1,12 +1,22 @@
 package pokedex;
+/*
+Grupo
+*/
 
 
 public class Pokedex {
+    /*
+            quais atributos um pokedex teria?
+        
+    */
     public static void main(String[] args) {
-        // Aqui vão ficar as ações do pokedex
-        //Ver pokemon
-        //pesquisar pokemon
-        //listar pokemon
+        //como a execução começa por aqui, pode ser um menu, que inicia com um metodo ligar que inicia os pokemons e as variavies e tudo necessario para o runtime
     }
-    
+    // Aqui vão ficar as ações do pokedex
+        
+        //Ver pokemon
+        
+        //pesquisar pokemon
+        
+        //listar pokemon
 }
