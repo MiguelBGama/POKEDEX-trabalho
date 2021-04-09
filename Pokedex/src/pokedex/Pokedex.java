1f5a8119d8087b7dd60a022733e06f3fa884c5da
Miguel <57196019+MiguelBGama@users.noreply.github.com>
2021-04-08 21:38:27 -0300

    Começo da criação do menu

--- Pokedex/src/pokedex/Pokedex.java
+++ Pokedex/src/pokedex/Pokedex.java
@@ -1,4 +1,7 @@
 package pokedex;
+
+import java.util.Scanner;
+
 /*
 Grupo 1: Leví Melo, Luiz Gustavo Pereira, Bruna Rosa Rodrigues e Miguel Bragança
 */
@@ -14,12 +17,21 @@ public class Pokedex {
     */
     public static void main(String[] args) {
         //como a execução começa por aqui, pode ser um menu, que inicia com um metodo ligar que inicia os pokemons e as variavies e tudo necessario para o runtime
+        Scanner scan = new Scanner(System.in);
+        Pokemon pokemon = new Pokemon();
+        int escolha = 0;
+        
+        do {
+            // Statements
+        }while(escolha == 0);
     }
     // Aqui vão ficar as ações do pokedex
         
-        //Ver pokemon
+        //Capturar pokemon
+    
+        //Treinar pokemon
         
-        //pesquisar pokemon
+        //ver meus pokemons
         
-        //listar pokemon
+        //informações pokemon
 }
