fad19f9da89b4f16415eac941325227494b39fa5
Miguel <57196019+MiguelBGama@users.noreply.github.com>
2021-04-09 21:08:30 -0300

    alterações do dia 9/4

--- Pokedex/src/pokedex/Pokedex.java
+++ Pokedex/src/pokedex/Pokedex.java
@@ -23,7 +23,44 @@ public class Pokedex {
         
         do {
             // Statements
-        }while(escolha == 0);
+            //menu que chama as funcionalidades
+            mostraMenu();
+            switch(escolha){               
+                case 1:    
+                 //code to be executed;    
+                 break;  //optional  
+                case 2:    
+                 //code to be executed;    
+                 break;  //optional   
+                case 3:    
+                 //code to be executed; 
+
+                default:     
+                    escolha = 0;
+                } 
+            
+               //Capturar pokemon
+    
+               //Treinar pokemon
+        
+               //ver meus pokemons
+        
+        //informações pokemon
+        }while(escolha != 4);
+    }
+    
+    public static void mostraMenu(){
+                System.out.print("\033[H\033[2J");  
+                System.out.flush(); 
+                System.out.println("|---------- POKEDEX ----------|");
+                System.out.println("|            MENU             |");
+                System.out.println("|                             |");
+                System.out.println("|     1- Capturar pokemon     |");
+                System.out.println("|     2- Treinar pokemon      |");
+                System.out.println("|     3- Ver pokemons         |");
+                System.out.println("|     4- Sair                 |");
+                System.out.println("|                             |");
+                System.out.println("|-----------------------------|");
     }
     // Aqui vão ficar as ações do pokedex
         
